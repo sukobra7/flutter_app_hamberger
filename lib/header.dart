3538a08f6b80f2ce5c9b4787572869eb49902b8b
kazumasa.nakamura <kazumasa.nakamura@vitalize.co.jp>
2021-07-06 23:34:01 +0900

    creating appbar

--- lib/header.dart
+++ lib/header.dart
@@ -14,22 +14,93 @@ class _HeaderState extends State<Header> {
     return SliverList(
       delegate: SliverChildListDelegate(
         [
-          Column(
-            children: [
-              Container(
-                height: size.height / 5,
-                decoration: BoxDecoration(
-                  color: Colors.teal,
-                  borderRadius: BorderRadius.vertical(
-                    bottom: Radius.circular(45),
+          //子のWidgetをStackの端から位置を指定して子を配置する
+          //Widgetを重ねる場合に使用します
+          Stack(children: [
+            Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 15),
+                  height: size.height / 5,
+                  decoration: BoxDecoration(
+                    color: Colors.teal,
+                    borderRadius: BorderRadius.vertical(
+                      bottom: Radius.circular(45),
+                    ),
+                    boxShadow: [
+                      BoxShadow(blurRadius: 2),
+                    ],
                   ),
-                  boxShadow: [
-                    BoxShadow(blurRadius: 2),
-                  ],
+                  child: Column(children: [
+                    SizedBox(height: 20),
+                    Row(children: [
+                      CircleAvatar(
+                        backgroundColor: Colors.white70,
+                        radius: 35,
+                        child: CircleAvatar(
+                          backgroundImage: AssetImage("images/kimura.jpeg"),
+                          radius: 30,
+                        ),
+                      ),
+                      SizedBox(width: 5),
+                      Column(
+                        children: [
+                          Text(
+                            'Kazumasa',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18),
+                          ),
+                          Container(
+                            padding: EdgeInsets.all(4),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.black54,
+                            ),
+                            child: Text(
+                              'GOLD MEMBER',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          )
+                        ],
+                      ),
+                      Spacer(),
+                      Text(
+                        "154 \$ can",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18),
+                      ),
+                    ]),
+                  ]),
                 ),
-              ),
-            ],
-          ),
+                SizedBox(height: 20),
+              ],
+            ),
+            Positioned(
+                bottom: 0,
+                child: Container(
+                  height: 50,
+                  width: size.width,
+                  child: Card(
+                    elevation: 3,
+                    margin: EdgeInsets.symmetric(horizontal: 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                          // 下線を削除
+                          border: InputBorder.none,
+                          labelText: "What dose your belly want to eat?",
+                          suffixIcon: Icon(Icons.search),
+                          contentPadding: EdgeInsets.only(left: 20)),
+                    ),
+                  ),
+                ))
+          ]),
         ],
       ),
     );
